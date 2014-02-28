99a2ef71152654ee059e204ae307ca5698443ee0
Ian Hawke <I.Hawke@soton.ac.uk>
2014-02-28 11:49:04 +0000

    Rewriting helper functions and reorganizing, in part to speed it up.

--- findhorizon.py
+++ findhorizon.py
@@ -1,5 +1,6 @@
 """
 Find Black Hole apparent horizons in an axisymmetric spacetime.
+===============================================================
 
 Black holes are usually described by their *horizon* enclosing the singularity.
 Locating any horizon in a general (typically numerically generated) spacetime
@@ -35,6 +36,7 @@ from scipy.optimize import brentq, root
 
 
 class spacetime:
+
     """
     Define an axisymmetric spacetime.
 
@@ -81,18 +83,19 @@ class spacetime:
         self.masses = np.array(masses)
         self.N = len(z_positions)
 
-        if reflection_symmetric:
-            assert np.all(np.array(z_positions) >= 0.0)
+#        if reflection_symmetric:
+#            assert np.all(np.array(z_positions) >= 0.0)
 
 
 class trappedsurface:
+
     r"""
     Store any trapped surface, centred on a particular point.
 
     The trapped surface is defined in polar coordinates centred on a point
     on the z-axis; the z-axis is :math:`\theta` = 0 or :math:`\theta` = 
     :math:`\pi`.
-    
+
     Parameters
     ----------
 
@@ -147,7 +150,7 @@ class trappedsurface:
     then in cartesians. Finally the horizon is plotted in the x-z plane.
     """
 
-    def __init__(self, spacetime, z_centre = 0.0):
+    def __init__(self, spacetime, z_centre=0.0):
         """
         Initialize a horizon centred on a particular point.
         """
@@ -177,11 +180,10 @@ class trappedsurface:
         z_i = self.spacetime.z_positions - self.z_centre
         m_i = self.spacetime.masses
 
-        y = np.array([h * np.sin(theta), h * np.cos(theta)])
         distance_i = np.zeros_like(z_i)
         for i in range(len(z_i)):
-            distance_i[i] = np.linalg.norm(y -
-                                           np.array([0, z_i[i]]), 2)
+            distance_i[i] = np.sqrt((h * np.sin(theta)) ** 2 +
+                                    (h * np.cos(theta) - z_i[i]) ** 2)
 
         C = 1.0 / np.sqrt(1.0 + (dhdtheta / h) ** 2)
         if (abs(theta) < 1e-16) or (abs(theta - np.pi) < 1e-16):
@@ -207,6 +209,74 @@ class trappedsurface:
             3.0 * dhdtheta ** 2 / h
 
         return dHdtheta
+
+    # Define the shooting function if using matching (0 <= theta <= pi)
+    def shooting_function_full(self, r0):
+        r"""
+        The function used in the shooting algorithm.
+
+        This is the full algorithm from integrating over
+        :math:`0 \le \theta \le \pi`. The difference between the 
+        solution and its derivative at the matching point is the
+        error to be minimized.
+
+        Parameters
+        ----------
+
+        r0 : list of float
+            Initial guess for the horizon radius, as outlined above.
+
+        Returns
+        -------
+
+        list of float
+            The error at the matching point.
+        """
+        # First half of the horizon
+        H0 = np.array([r0[0], 0.0])
+        solver1 = ode(self.expansion)
+        solver1.set_integrator("dopri5", atol=1.e-8, rtol=1.e-6)
+        solver1.set_initial_value(H0, 0.0)
+        solver1.integrate(np.pi / 2.0)
+        # Second half of the horizon
+        H0 = np.array([r0[1], 0.0])
+        solver2 = ode(self.expansion)
+        solver2.set_integrator("dopri5", atol=1.e-8, rtol=1.e-6)
+        solver2.set_initial_value(H0, np.pi)
+        solver2.integrate(np.pi / 2.0)
+
+        return solver1.y - solver2.y
+
+    # Define the shooting function if symmetric (0 <= theta <= pi/2)
+    def shooting_function(self, r0):
+        r"""
+        The function used in the shooting algorithm.
+
+        This is the symmetric algorithm from integrating over
+        :math:`0 \le \theta \le \pi / 2`. The difference between the 
+        derivative at the end point and the boundary condition is the
+        error to be minimized.
+
+        Parameters
+        ----------
+
+        r0 : float
+            Initial guess for the horizon radius, as outlined above.
+
+        Returns
+        -------
+
+        float
+            The error at the end point.
+        """
+
+        H0 = np.array([r0, 0.0])
+        solver1 = ode(self.expansion)
+        solver1.set_integrator("dopri5", atol=1.e-8, rtol=1.e-6)
+        solver1.set_initial_value(H0, 0.0)
+        solver1.integrate(np.pi / 2.0)
+
+        return solver1.y[1]
 
     def find_r0(self, input_guess, full_horizon=False):
         r"""
@@ -243,82 +313,13 @@ class trappedsurface:
             find a trapped surface surrounding just one singularity.
         """
 
-        # Define the shooting function if using matching (0 <= theta <= pi)
-        def shooting_function_full(r0):
-            r"""
-            The function used in the shooting algorithm.
-
-            This is the full algorithm from integrating over
-            :math:`0 \le \theta \le \pi`. The difference between the 
-            solution and its derivative at the matching point is the
-            error to be minimized.
-
-            Parameters
-            ----------
-
-            r0 : list of float
-                Initial guess for the horizon radius, as outlined above.
-
-            Returns
-            -------
-            
-            list of float
-                The error at the matching point.
-            """
-
-            # First half of the horizon
-            H0 = np.array([r0[0], 0.0])
-            solver1 = ode(self.expansion)
-            solver1.set_integrator("dopri5", atol=1.e-8, rtol=1.e-6)
-            solver1.set_initial_value(H0, 0.0)
-            solver1.integrate(np.pi / 2.0)
-            # Second half of the horizon
-            H0 = np.array([r0[1], 0.0])
-            solver2 = ode(self.expansion)
-            solver2.set_integrator("dopri5", atol=1.e-8, rtol=1.e-6)
-            solver2.set_initial_value(H0, np.pi)
-            solver2.integrate(np.pi / 2.0)
-
-            return solver1.y - solver2.y
-
-        # Define the shooting function if symmetric (0 <= theta <= pi/2)
-        def shooting_function(r0):
-            r"""
-            The function used in the shooting algorithm.
-
-            This is the symmetric algorithm from integrating over
-            :math:`0 \le \theta \le \pi / 2`. The difference between the 
-            derivative at the end point and the boundary condition is the
-            error to be minimized.
-
-            Parameters
-            ----------
-
-            r0 : float
-                Initial guess for the horizon radius, as outlined above.
-
-            Returns
-            -------
-            
-            float
-                The error at the end point.
-            """
-
-            H0 = np.array([r0, 0.0])
-            solver1 = ode(self.expansion)
-            solver1.set_integrator("dopri5", atol=1.e-8, rtol=1.e-6)
-            solver1.set_initial_value(H0, 0.0)
-            solver1.integrate(np.pi / 2.0)
-
-            return solver1.y[1]
-
         # Now find the horizon given the input guess
         self.r0 = []
         if (full_horizon or not self.spacetime.reflection_symmetric):
-            sol = root(shooting_function_full, input_guess, tol=1.e-12)
+            sol = root(self.shooting_function_full, input_guess, tol=1.e-12)
             self.r0 = sol.x
         else:
-            sol = brentq(shooting_function, input_guess[0],
+            sol = brentq(self.shooting_function, input_guess[0],
                          input_guess[1])
             self.r0 = [sol]
 
@@ -444,238 +445,85 @@ class trappedsurface:
 
         return None
 
+    def plot_2d(self, ax):
+        """
+        Given a matplotlib axis, plot the trapped surface.
 
-def expansion(theta, H, params):
-    """
-    Compute the expansion at a single point.
-    """
+        Plots the surface in the x-z plane, together with the location of
+        the singularities: marker style is used to indicate the mass of
+        the singularity (will fail badly for masses significantly larger
+        than 1).
 
-    h = H[0]
-    dhdtheta = H[1]
+        Parameters
+        ----------
 
-    singularities = params[0]
+        ax : axis object
+            Matplotlib axis on which to do the plot.
+        """
 
-    m_i = singularities['masses']
-    z_i = singularities['z_positions']
-
-    y = np.array([h * np.sin(theta), h * np.cos(theta)])
-    distance_i = np.zeros_like(z_i)
-    for i in range(len(z_i)):
-        distance_i[i] = np.linalg.norm(y - np.array([0, z_i[i]]), 2)
-
-    C = 1.0 / np.sqrt(1.0 + (dhdtheta / h) ** 2)
-    if (abs(theta) < 1e-16) or (abs(theta - np.pi) < 1e-16):
-        cot_theta_dhdtheta_C2 = 0.0
-    else:
-        cot_theta_dhdtheta_C2 = dhdtheta / (np.tan(theta) * C ** 2)
-
-    psi = 1.0
-    dpsi_dr = 0.0
-    dpsi_dtheta = 0.0
-    for i in range(len(m_i)):
-        psi += 0.5 * m_i[i] / distance_i[i]
-        dpsi_dr += 0.5 * m_i[i] * (z_i[i] * np.cos(theta) - h) / \
-            distance_i[i] ** 3
-        dpsi_dtheta += 0.5 * m_i[i] * h * (-z_i[i] * np.sin(theta)) / \
-            distance_i[i] ** 3
-
-    dHdtheta = np.zeros_like(H)
-    dHdtheta[0] = dhdtheta
-    dHdtheta[1] = 2.0 * h - cot_theta_dhdtheta_C2 + \
-        4.0 * h ** 2 / (psi * C ** 2) * \
-        (dpsi_dr - dpsi_dtheta * dhdtheta / h ** 2) + \
-        3.0 * dhdtheta ** 2 / h
-
-    return dHdtheta
-
-
-def shooting_function(h0, singularities, theta0=0.0, theta_end=np.pi / 2.0):
-    """
-    Find the horizon using shooting. Returns the error at the endpoint.
-    """
-
-    theta_out = []
-    H = []
-    H_0 = np.array([h0, 0.0])
-    solver = ode(expansion)
-    solver.set_integrator("dopri5", atol=1.e-8, rtol=1.e-6)
-    solver.set_initial_value(H_0, 0.0)
-    solver.set_f_params(singularities)
-    theta_out.append(theta0)
-    H.append(H_0)
-
-    dtheta = (theta_end - theta0) / 10.0
-
-    while solver.successful() and solver.t < theta_end:
-        solver.integrate(solver.t + dtheta)
-
-        H.append(solver.y)
-        theta_out.append(solver.t)
-
-    return solver.y[1]
-
-
-def shooting_function_full(h0, singularities,
-                           theta0=0.0, theta_end=np.pi,
-                           theta_match=np.pi / 2.0):
-    """
-    Find the horizon using shooting. Returns the matching error.
-    """
-
-    H_0 = np.array([h0[0], 0.0])
-    solver1 = ode(expansion)
-    solver1.set_integrator("dopri5", atol=1.e-8, rtol=1.e-6)
-    solver1.set_initial_value(H_0, 0.0)
-    solver1.set_f_params(singularities)
-
-    dtheta = (theta_match - theta0) / 10.0
-
-    while solver1.successful() and solver1.t < theta_match:
-        solver1.integrate(solver1.t + dtheta)
-
-    H_0 = np.array([h0[1], 0.0])
-    solver2 = ode(expansion)
-    solver2.set_integrator("dopri5", atol=1.e-8, rtol=1.e-6)
-    solver2.set_initial_value(H_0, theta_end)
-    solver2.set_f_params(singularities)
-
-    dtheta = (theta_match - theta_end) / 10.0
-
-    while solver2.successful() and solver2.t > theta_match:
-        solver2.integrate(solver2.t + dtheta)
-
-    return solver1.y - solver2.y
-
-
-def FindHorizonFull(input_guess, singularities, options=None):
-    """
-    Find horizons given N singularities (positions and locations),
-    assuming
-    1) conformal flatness
-    2) axisymmetry
-    The input_guess is the radius on the axis.
-    """
-
-    # Find the horizon
-
-    sol = root(shooting_function_full, input_guess, args=([singularities],))
-    h0 = sol.x
-
-    theta1 = []
-    H1 = []
-    H_0 = np.array([h0[0], 0.0])
-    solver1 = ode(expansion)
-    solver1.set_integrator("dopri5", atol=1.e-8, rtol=1.e-6)
-    solver1.set_initial_value(H_0, 0.0)
-    solver1.set_f_params([singularities])
-    theta1.append(0.0)
-    H1.append(H_0)
-
-    dtheta = np.pi / 100.0
-
-    while solver1.successful() and solver1.t < np.pi / 2.0:
-        solver1.integrate(solver1.t + dtheta)
-
-        H1.append(solver1.y)
-        theta1.append(solver1.t)
-
-    theta2 = []
-    H2 = []
-    H_0 = np.array([h0[1], 0.0])
-    solver2 = ode(expansion)
-    solver2.set_integrator("dopri5", atol=1.e-8, rtol=1.e-6)
-    solver2.set_initial_value(H_0, np.pi)
-    solver2.set_f_params([singularities])
-    theta2.append(np.pi)
-    H2.append(H_0)
-
-    dtheta = -np.pi / 100.0
-
-    while solver2.successful() and solver2.t > np.pi / 2.0:
-        solver2.integrate(solver2.t + dtheta)
-
-        H2.append(solver2.y)
-        theta2.append(solver2.t)
-
-    H = np.vstack((np.array(H1), np.flipud(np.array(H2))))
-    theta_out = np.hstack((np.array(theta1), np.flipud(np.array(theta2))))
-
-    return theta_out, H
-
-
-def FindHorizonSymmetric(input_guess, singularities, options=None):
-    """
-    Find horizons given N singularities (positions and locations),
-    assuming
-    1) conformal flatness
-    2) axisymmetry
-    The input_guess is the radius on the axis at both ends.
-    """
-
-    # Find the horizon
-
-    h0 = brentq(shooting_function, input_guess[
-                0], input_guess[1], args=([singularities],))
-
-    theta_out = []
-    H = []
-    H_0 = np.array([h0, 0.0])
-    solver = ode(expansion)
-    solver.set_integrator("dopri5", atol=1.e-8, rtol=1.e-6)
-    solver.set_initial_value(H_0, 0.0)
-    solver.set_f_params([singularities])
-    theta_out.append(0.0)
-    H.append(H_0)
-
-    dtheta = np.pi / 100.0
-
-    while solver.successful() and solver.t < np.pi / 2.0:
-        solver.integrate(solver.t + dtheta)
-
-        H.append(solver.y)
-        theta_out.append(solver.t)
-
-    H = np.array(H)
-    theta_out = np.array(theta_out)
-
-    return theta_out, H
+        ax.plot(self.x, self.z, 'b-')
+        for z, m in zip(self.spacetime.z_positions, self.spacetime.masses):
+            ax.plot(0.0, z,
+                    'kx', markersize=12, markeredgewidth=1 + int(round(m)))
+        ax.set_xlabel("$x$")
+        ax.set_ylabel("$z$")
+        ax.axis('equal')
 
 
 def FindHorizonBinarySymmetric(z=0.5, mass=1.0):
-    """
-    Find the common horizon of the axisymmetric system with reflection symmetry
-    with singularities at \pm z with equal bare mass.
+    r"""
+    Utility function to find horizons for reflection symmetric case.
+
+    This returns the horizon for a spacetime with precisely two singularities
+    of identical mass located at :math:`\pm z`.
+
+    Notes
+    -----
+
+    The initial guess for the horizon location is based on fitting a cubic
+    to the results constructed for :math:`0 \le z \le 0.75` for the unit
+    mass case. The radius should scale with the mass. For larger separations
+    we should not expect a common horizon.
+
+    Parameters
+    ----------
+
+    z : float, optional
+        The distance from the origin of the singularities (ie the two
+        singularities are located at [-z, +z]).
+    mass : float, optional
+        The mass of the singularities.
+
+    Returns
+    -------
+
+    ts : trappedsurface
+        Only returns the single surface found, expected to be the common
+        horizon.
     """
 
-    singularities = {"masses": np.array([mass, mass]),
-                     "z_positions": np.array([-z, z])}
+    st = spacetime([-z, z], [mass, mass], True)
+    ts = trappedsurface(st, 0.0)
     # An empirical formula for the required initial guess
     # (ie the value of r0, or h, at theta = 0)
-    r0_empirical = mass * (0.976 + 0.2 * z + 0.27 * z ** 2)
-    initial_guess = [0.95 * r0_empirical, 1.05 * r0_empirical]
+    r0_empirical = mass * (1.0 - 0.0383 * z + 0.945 * z ** 2 - 0.522 * z ** 3)
+    # This empirical formula works for the inner horizon if
+    # 0.65 < z < 0.72 or so. There is an inner horizon findable
+    # down to about 0.47, but the initial guess is very sensitive
+    # r0_empirical = mass * (0.204 - 1.6422*z - 0.771*z**2 + 0.5*z**3)
+    initial_guess = [0.99 * r0_empirical, 1.01 * r0_empirical]
     try:
-        theta, H = FindHorizonSymmetric(initial_guess, singularities)
+        ts.find_r0(initial_guess)
     except ValueError:
-        r0 = np.linspace(initial_guess[0], initial_guess[1])
+        r0 = np.linspace(0.95 * r0_empirical, 1.05 * r0_empirical)
         phi = np.zeros_like(r0)
         for i in range(len(r0)):
-            phi[i] = shooting_function(r0[i], [singularities])
+            phi[i] = ts.shooting_function(r0[i])
         initial_guess = [r0[np.argmin(phi)], r0[-1]]
-        theta, H = FindHorizonSymmetric(initial_guess, singularities)
-    return theta, H
-
-
-def PlotHorizon2d(ax, theta, H, z_positions, masses):
-    """
-    Given all theta and H values, plot the x-z plane cut.
-    """
-    ax.plot(H[:, 0] * np.sin(theta), H[:, 0] * np.cos(theta), 'b-')
-    for z, m in zip(z_positions, masses):
-        ax.plot(0.0, z,
-                'kx', markersize=12, markeredgewidth=1 + int(round(m)))
-    ax.set_xlabel("$x$")
-    ax.set_ylabel("$z$")
-    ax.axis('equal')
+        ts.find_r0(initial_guess)
+    ts.solve_given_r0()
+    ts.convert_to_cartesian()
+    return ts
 
 
 def PlotHorizon3d(ax, theta, H):
@@ -722,68 +570,9 @@ def PlotHorizonInteractive3d(ax, theta, H):
     mlab.show()
 
 
-def PlotHorizonSymmetric(theta, H, z=0.5, mass=1.0, elev=None, azim=None):
-    """
-    Take the output for a symmetric horizon
-    (ie, theta \in [0, pi/2]) and plot it using 2 and 3d figures.
-    """
-    import matplotlib.pyplot as plt
-    #from mpl_toolkits.mplot3d import Axes3D
-    fig = plt.figure(figsize=(12, 8))
-    ax1 = fig.add_subplot(121)
-    half_theta = np.hstack((theta, np.flipud(np.pi - theta)))
-    half_H = np.vstack((H, np.flipud(H)))
-    all_theta = np.hstack((half_theta, half_theta + np.pi))
-    all_H = np.vstack((half_H, half_H))
-    PlotHorizon2d(
-        ax1, all_theta, all_H, np.array([-z, z]), np.array([mass, mass]))
-    ax1.set_title("Singularities at $z = \pm {}$".format(z))
-    ax2 = fig.add_subplot(122, projection='3d')
-    PlotHorizon3d(ax2, all_theta, all_H)
-    if elev is not None:
-        ax2.elev = elev
-    if azim is not None:
-        ax2.azim = azim
-    ax2.set_title("Singularities at $z = \pm {}$".format(z))
-    plt.show()
-
-
-def SolvePlotSymmetric(z=0.5, mass=1.0, elev=None, azim=None):
-
-    theta, H = FindHorizonBinarySymmetric(z, mass)
-    PlotHorizonSymmetric(theta, H, z, mass, elev, azim)
-
-
-def SolvePlotASymmetric(z=[-0.5, 0.5], mass=[1.0, 1.0],
-                        elev=None, azim=None):
-    """
-    Calculate and plot the results even in the full case.
-    """
-
-    singularities = {"masses": np.array(mass),
-                     "z_positions": np.array(z)}
-    r0_empirical = np.sum(mass) * (0.97 + 0.2 * np.max(np.abs(z)) +
-                                   0.27 * np.max(np.abs(z)) ** 2)
-    input_guess = [r0_empirical, r0_empirical]
-    theta, H = FindHorizonFull(input_guess, singularities)
-    all_theta = np.hstack((theta, theta + np.pi))
-    all_H = np.vstack((H, np.flipud(H)))
-    import matplotlib.pyplot as plt
-    #from mpl_toolkits.mplot3d import Axes3D
-    fig = plt.figure(figsize=(12, 8))
-    ax1 = fig.add_subplot(121)
-    PlotHorizon2d(
-        ax1, all_theta, all_H, np.array(z), np.array(mass))
-    ax1.set_title("Singularities at $z = \pm {}$".format(z))
-    ax2 = fig.add_subplot(122, projection='3d')
-    PlotHorizon3d(ax2, all_theta, all_H)
-    if elev is not None:
-        ax2.elev = elev
-    if azim is not None:
-        ax2.azim = azim
-    ax2.set_title("Singularities at $z = \pm {}$".format(z))
-    plt.show()
-
-
 if __name__ == "__main__":
-    SolvePlotSymmetric()
+    # SolvePlotSymmetric()
+    st = spacetime([-0.5, 0.5], [1.0, 1.0])
+    ts = trappedsurface(st)
+    ts.find_r0([1.0, 1.0])
+    ts.solve_given_r0()
